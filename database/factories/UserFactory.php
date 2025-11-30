7869f9829f20e030e43bfe43d3e2e4e58643973b
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-11-30 09:44:33 -0700

    DEKLARASI FACTORYING

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,23 +11,20 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement(['murid', 'mentor', 'admin']),
+            'status' => fake()->randomElement(['aktif', 'non-aktif']),
+            'deskripsi' => fake()->paragraph(),
             'remember_token' => Str::random(10),
         ];
     }
